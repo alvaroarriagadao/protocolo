77841a5056e65762361b0bb1e8f0f0188f90d5ed
Alvaro Arriagada Ortega <alvaroarriagadaortega@gmail.com>
2026-06-01 22:55:06 -0400

    fix: add proper service worker with correct cache path

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'protocolo-v3';
+const URL_TO_CACHE = '/protocolo/';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.add(URL_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(URL_TO_CACHE)))
+  );
+});
